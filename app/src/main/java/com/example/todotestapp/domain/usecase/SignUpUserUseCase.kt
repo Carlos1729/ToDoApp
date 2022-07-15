ea57a03daa204414d2b8edcb7f8b0950feb9ee6a
madhukarkolli <kolli.madhukar@byjus.com>
2022-07-16 00:29:54 +0530

    login sign up implemented and removed bugs of bottomsheet

--- app/src/main/java/com/example/todotestapp/domain/usecase/SignUpUserUseCase.kt
+++ app/src/main/java/com/example/todotestapp/domain/usecase/SignUpUserUseCase.kt
@@ -1,15 +1,14 @@
 package com.example.todotestapp.domain.usecase
 
-import com.example.todotestapp.data.db.BaseResponse
 import com.example.todotestapp.data.db.LoginResponse
-import com.example.todotestapp.data.db.SignUpUserModel
+import com.example.todotestapp.data.db.SignUpUserRequest
+import com.example.todotestapp.data.db.SignUpUserResponse
 import com.example.todotestapp.domain.repositoryinterface.ToDoRepository
 import retrofit2.Response
 
-class SignUpUserUseCase (private val todoRepo: ToDoRepository){
+class SignUpUserUseCase(private val todoRepo: ToDoRepository) {
 
-//    suspend fun signUpUserByDetails(requestBody: SignUpUserModel): Response<BaseResponse<SignUpUserModel>> {
-//        return todoRepo.signUpUser(requestBody)
-//    }
-
+    suspend fun SignUpUserByDetails(userEN: SignUpUserRequest): Response<SignUpUserResponse> {
+        return todoRepo.signUpUser(userEN)
+    }
 }
